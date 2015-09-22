50701b454508f68390410f9f9bee33c9e28b4b3a
Eric Moritz <ericmoritz@gmail.com>
2015-09-22 12:37:49 -0400

    DL-138 Superficial version bump to override the existing cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'emoritz@gannett.com'
 license          'All rights reserved'
 description      'Installs/Configures mapr-spark'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.2'
 
 depends 'mapr_installation', '~> 0.3'
